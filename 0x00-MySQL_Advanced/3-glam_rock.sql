bf5f4b1e973f62c10ff55caea08b18044809ff37
ismailouzy <ismailouzal@gmail.com>
2024-08-08 21:58:23 +0000

    done

---
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,6 @@
+-- Script to list Glam Rock bands ranked by their longevity
+-- Selecting band name and calculating lifespan based on 'formed' and 'split'
+-- We assume bands that haven't split yet will be considered as active in 2022
+
+SELECT band_name, COALESCE(split, 2022) - formed as life FROM metal_bands
+WHERE style LIKE '%Glam rock%' ORDER BY life DiESC;
