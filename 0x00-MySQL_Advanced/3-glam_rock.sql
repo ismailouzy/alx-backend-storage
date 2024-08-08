3f3a1463e7d66aea50e47235f3c3e66989db3ea1
ismailouzy <ismailouzal@gmail.com>
2024-08-08 22:00:03 +0000

    try lifespan

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -2,16 +2,5 @@
 -- Selecting band name and calculating lifespan based on 'formed' and 'split'
 -- We assume bands that haven't split yet will be considered as active in 2022
 
-
-SELECT
-    band_name,
-    CASE
-        WHEN split IS NULL THEN 2022 - formed
-        ELSE split - formed
-    END AS lifespan
-FROM
-    metal_bands
-WHERE
-    main_style = 'Glam Rock'
-ORDER BY
-    lifespan DESC;
+SELECT band_name, COALESCE(split, 2022) - formed as lifespan FROM metal_bands
+WHERE style LIKE '%Glam rock%' ORDER BY lifespan DESC;
